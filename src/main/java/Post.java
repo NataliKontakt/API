a6b4780b7200032c1140902907fb0f401fec4251
“Natali <natali-kontakt@yandex.ru>
2022-10-06 22:06:19 +0500

    add Post

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public FormDate birthday; // поле для даты рождения
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для согласия получать новости
+
+}
